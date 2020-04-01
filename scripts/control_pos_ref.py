f1ea35e46f60a67893b3a4237784d85921148812
David Wannheden <david@wannheden.com>
2020-04-01 14:38:48 +0200

    implemented control_pos_ref.py for right arm

--- scripts/control_pos_ref.py
+++ scripts/control_pos_ref.py
@@ -5,69 +5,157 @@
 # Currently full of pseudocode but is a shell of how the final control node is going to work
 
 import rospy
-from std_msgs.msg import String
 from std_msgs.msg import Float64
+from geometry_msgs.msg import *
+from robot_kinematic_services.srv import ForwardKinematics
+from robot_kinematic_services.srv import TaskSpaceKinematics
+from sensor_msgs.msg import JointState
 
-#attempt at using class for subscriber
+Kp = 3.5
+# Ki = 0.02
+# I_r = [0, 0, 0, 0, 0, 0, 0]  # integral part of controller
+# I_l = [0, 0, 0, 0, 0, 0, 0]  # first three slots are x, y, z position and last 4 x, y, z, w quaternion
+
+
 class Subscriber(object):
     def __init__(self):
-        self.pub = rospy.Subscriber('yumi', String, self.sub_callback)
-        self.current_pose = ()  #dont know yet what the message looks like
+        self.sub = rospy.Subscriber('/joint_states', JointState, self.sub_callback)
+        self.current_state = None
 
     def sub_callback(self, msg):
-        self.current_pose = msg
+        self.current_state = msg
 
 
+def run(goal_poses_r, goal_poses_l):
+    # Set loop frequency
+    rate = rospy.Rate(10)
+
+    # Initialize publishers
+    # Note: The order in which these are appended to the list matters! (1, 2, 7, 3, 4, 5, 6)
+    publishers = list()
+
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_1_r/command', Float64, queue_size=1))
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_2_r/command', Float64, queue_size=1))
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_7_r/command', Float64, queue_size=1))
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_3_r/command', Float64, queue_size=1))
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_4_r/command', Float64, queue_size=1))
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_5_r/command', Float64, queue_size=1))
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_6_r/command', Float64, queue_size=1))
+
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_1_l/command', Float64, queue_size=1))
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_2_l/command', Float64, queue_size=1))
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_7_l/command', Float64, queue_size=1))
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_3_l/command', Float64, queue_size=1))
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_4_l/command', Float64, queue_size=1))
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_5_l/command', Float64, queue_size=1))
+    publishers.append(rospy.Publisher('/yumi/joint_vel_controller_6_l/command', Float64, queue_size=1))
 
 
-def run(goal_pose):
-    pub1 = rospy.Publisher('/yumi/joint_vel_controller_1_r/command', Float64, queue_size=1)  # initiate publishers, incomplete
-    pub2 = rospy.Publisher('/yumi/joint_vel_controller_2_r/command', Float64, queue_size=1)
-    pub3 = rospy.Publisher('/yumi/joint_vel_controller_3_r/command', Float64, queue_size=1)
-    pub4 = rospy.Publisher('/yumi/joint_vel_controller_4_r/command', Float64, queue_size=1)
-    pub5 = rospy.Publisher('/yumi/joint_vel_controller_5_r/command', Float64, queue_size=1)
-    pub6 = rospy.Publisher('/yumi/joint_vel_controller_6_r/command', Float64, queue_size=1)
-    pub7 = rospy.Publisher('/yumi/joint_vel_controller_7_r/command', Float64, queue_size=1)
+    # MISSING: GRIPPER PUBLISHERS
+
+    # initialize subscriber
     subscriber = Subscriber()
-    rospy.init_node('control_node', anonymous=True)  # initiate node
-    rate = rospy.Rate(10)  # 10hz
+
+    # LOOP
     while not rospy.is_shutdown():
         rospy.spin
-        while subscriber.current_pose is None: #for when node has just been started up
+        while subscriber.current_state is None: #for when node has just been started up
             rospy.spin
-        current_velocities = get_current_velocities(goal_pose, subscriber.current_pose)
+        # only handling SX for now
+        velocities_r = compute_ts("gripper_r_base", subscriber.current_state, Kp, goal_poses_r[0].pose)
+
+        # Publish the new velocities
+        for i in range(7):
+            publishers[i].publish(velocities_r[i])
+            print("published " + str(velocities_r[i]) + " onto " + str(publishers[i].name))
 
 
-        pub1.publish(current_velocities(0)) #rough example of how publications are gonnawork
-        pub2.publish(current_velocities(1))
-        # rospy.loginfo(current_velocities)
 
         rate.sleep()
 
-#def callback(data): #obsolete?
-#   rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+def get_input_r():
+
+    # Empty for now
+    desired_poses = []
+    for i in range(1, 2):
+        desired_poses.append(PoseStamped())
+        desired_poses[i-1].header.frame_id = "yumi_base_link"
+        desired_poses[i-1].header.stamp = rospy.Time.now()
+        desired_poses[i-1].pose.position.x = rospy.get_param('/posestamp' + str(i+1) + '/pose/position/x')
+        desired_poses[i-1].pose.position.y = rospy.get_param('/posestamp' + str(i+1) + '/pose/position/y')
+        desired_poses[i-1].pose.position.z = rospy.get_param('/posestamp' + str(i+1) + '/pose/position/z')
+        desired_poses[i-1].pose.orientation.x = rospy.get_param('/posestamp' + str(i+1) + '/pose/orientation/x')
+        desired_poses[i-1].pose.orientation.y = rospy.get_param('/posestamp' + str(i+1) + '/pose/orientation/y')
+        desired_poses[i-1].pose.orientation.z = rospy.get_param('/posestamp' + str(i+1) + '/pose/orientation/z')
+        desired_poses[i-1].pose.orientation.w = rospy.get_param('/posestamp' + str(i+1) + '/pose/orientation/w')
+
+    return desired_poses
+
+
+def get_input_l():
+
+    # Parse YAML data into array of Posestamps
+    # Data is stored in /cth_yumi/config/..
+    desired_poses = []
+    for i in range(1):
+        desired_poses.append(PoseStamped())
+        desired_poses[i].header.frame_id = "yumi_base_link"
+        desired_poses[i].header.stamp = rospy.Time.now()
+        desired_poses[i].pose.position.x = rospy.get_param('/posestamp' + str(i+1) + '/pose/position/x')
+        desired_poses[i].pose.position.y = rospy.get_param('/posestamp' + str(i+1) + '/pose/position/y')
+        desired_poses[i].pose.position.z = rospy.get_param('/posestamp' + str(i+1) + '/pose/position/z')
+        desired_poses[i].pose.orientation.x = rospy.get_param('/posestamp' + str(i+1) + '/pose/orientation/x')
+        desired_poses[i].pose.orientation.y = rospy.get_param('/posestamp' + str(i+1) + '/pose/orientation/y')
+        desired_poses[i].pose.orientation.z = rospy.get_param('/posestamp' + str(i+1) + '/pose/orientation/z')
+        desired_poses[i].pose.orientation.w = rospy.get_param('/posestamp' + str(i+1) + '/pose/orientation/w')
+
+    return desired_poses
 
 
 
-def get_input():
-    # for now generates an empty pose and returns it
-    # maybe should take in a pose and apply a header onto it here
-    pose = PoseStamped()
-    return pose
+def compute_ts(chain_end_effector_name, current_state, kp, goal_pose):
+    current_velocities = []
+    computets = rospy.ServiceProxy('/compute_ts', TaskSpaceKinematics)
+    solution = computets(chain_end_effector_name, "", kp, current_state, goal_pose)
 
+    if solution.status.code == 0:
+        new_state = solution.ts_solution
+        print("TS COMPUTATION WORKED. ANSWER IS ")
+        print(new_state)
 
+        for i in range(7):
+            current_velocities.append(new_state.velocity[i])
 
-
-def get_current_velocities(goal_pose, current_pose): #P-controller, not implemented, returns array of velocities
-    #should jacobian compution occur here or in another method? are we going to use a service node from Diogo?
-    current_velocities = ()
     return current_velocities
 
-if __name__ == '__main__':
-    # this should be looped so that code runs from here when new input is detected
-    goal_pose = get_input()  # input is of type PoseStamped
 
+# maybe useless?
+def compute_fk(current_state, chain_end_effector_name):
+
+    # call diogos F_K service node, send current state and return current pose
+    # service node needs to be running
+    computefk = rospy.ServiceProxy('/compute_fk', ForwardKinematics)
+    solution = computefk(chain_end_effector_name, "", current_state)  # this service requires two strings, dont know what the second is
+
+    if solution.status.code == 0:  # check if all computations worked
+        current_pose = solution.fk_solution  # TYPE POSESTAMPED
+        print("FK COMPUTATION WORKED. ANSWER IS ")
+        print(current_pose)
+    else:
+        print("FK COMPUTATION FAILED")
+
+    return current_pose
+
+
+if __name__ == '__main__':
+    # Initiate node
+    rospy.init_node('control_node', anonymous=True)
+
+    goal_poses_r = get_input_r()
+    goal_poses_l = get_input_l()
+    # Run
     try:
-        run(goal_pose)
+        run(goal_poses_r, goal_poses_l)
     except rospy.ROSInterruptException:
         pass
