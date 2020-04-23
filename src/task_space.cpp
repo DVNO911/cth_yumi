8de8c7bf15982632eb5649b184f84b115f45e645
David Wannheden <david@wannheden.com>
2020-04-23 11:33:52 +0200

    in task_space the left arm now moves relative to the right arm

--- src/task_space.cpp
+++ src/task_space.cpp
@@ -7,9 +7,12 @@
 
 
 sensor_msgs::JointState state; // The current state as recorded by the subscriber 
-float Kp = 1.5; 
-std::string chain_end_effector_name_r = "gripper_r_base"; // Kept as global variables because it might be useful to use other frames as reference
-std::string chain_end_effector_name_l = "gripper_l_base";
+float Kp = 3.5; 
+std::string chain_end_effector_name_r = "gripper_r_finger_l"; // Kept as global variables because it might be useful to use other frames as reference
+std::string chain_end_effector_name_l = "gripper_l_finger_r";
+
+int counter = 0;
+int pose_counter = 0;
 
 // Subscriber 
 void stateCb(const sensor_msgs::JointState::ConstPtr &msg)
@@ -18,18 +21,49 @@ void stateCb(const sensor_msgs::JointState::ConstPtr &msg)
 }
 
 
-KDL::Vector getDesiredPosition_r(){
+KDL::Vector getDesiredPosition_r1(){
   KDL::Vector desired_position;
   // TO DO: Parse from YAMLfile
   desired_position.x(0.288638);
   desired_position.y(-0.328583);
+  desired_position.z(0.478045);  
+  return desired_position;
+}
+
+  KDL::Vector getDesiredPosition_r2(){
+  KDL::Vector desired_position;
+  desired_position.x(0.288638);
+  desired_position.y(-0.228583);
+  desired_position.z(0.278045);
+return desired_position;
+  }
+
+KDL::Vector getDesiredPosition_r3(){
+  KDL::Vector desired_position;
+  desired_position.x(0.288638);
+  desired_position.y(-0.328583);
+  desired_position.z(0.378045);
+  return desired_position;
+}
+
+KDL::Vector getDesiredPosition_l1(){
+  KDL::Vector desired_position;
+  // TO DO: Parse from YAMLfile
+  desired_position.x(0.288638);
+  desired_position.y(0.328583);
+  desired_position.z(0.378045);
+  return desired_position;
+}
+KDL::Vector getDesiredPosition_l2(){
+  KDL::Vector desired_position;
+  desired_position.x(0.288638);
+  desired_position.y(0.128583);
   desired_position.z(0.478045);
   return desired_position;
 }
 
-KDL::Vector getDesiredPosition_l(){
+KDL::Vector getDesiredPosition_l3(){
   KDL::Vector desired_position;
-  // TO DO: Parse from YAMLfile
   desired_position.x(0.288638);
   desired_position.y(0.328583);
   desired_position.z(0.378045);
@@ -72,15 +106,21 @@ int main (int argc, char ** argv)
   manager.initializeArm(chain_end_effector_name_l);
 
   KDL::Frame pose_r, pose_l;
-  KDL::Vector desired_position_r, desired_position_l, error_r, error_l;
+  KDL::Vector desired_position_r1, desired_position_l1, desired_position_r2, desired_position_l2, desired_position_r3, desired_position_l3, error_r, error_l;
   KDL::JntArray q_dot_r(7), q_dot_l(7);
   KDL::Twist command_vel_r = KDL::Twist::Zero();
   KDL::Twist command_vel_l = KDL::Twist::Zero();
   sensor_msgs::JointState command_r, command_l;
 
-  // Parse goal pose
-  desired_position_r = getDesiredPosition_r();
-  desired_position_l = getDesiredPosition_l();
+  // Parse goal poses
+  desired_position_r1 = getDesiredPosition_r1();
+  desired_position_l1 = getDesiredPosition_l1();
+
+  desired_position_r2 = getDesiredPosition_r2();
+  desired_position_l2 = getDesiredPosition_l2();
+
+  desired_position_r3 = getDesiredPosition_r3();
+  desired_position_l3 = getDesiredPosition_l3();
 
 
   // LOOP
@@ -91,8 +131,34 @@ int main (int argc, char ** argv)
     {
       manager.getEefPose(chain_end_effector_name_l, state, pose_l);
       ROS_INFO_STREAM("getEefPose computed");
-      error_r = desired_position_r - pose_r.p;
-      error_l = desired_position_l - pose_l.p;
+
+      if(pose_counter == 0){
+      error_r = desired_position_r1 - pose_r.p;
+      // error_l = desired_position_l1 - pose_l.p;
+
+      KDL::Frame pose_temp = pose_r;
+      pose_temp.p.y(0);
+      error_l = pose_temp.p - pose_l.p;
+      }
+
+      else if (pose_counter == 1){
+      error_r = desired_position_r2 - pose_r.p;
+      // error_l = desired_position_l2 - pose_l.p;
+      
+      KDL::Frame pose_temp = pose_r;
+      pose_temp.p.y(0);
+      error_l = pose_temp.p - pose_l.p;
+      } 
+
+      else{
+      error_r = desired_position_r3 - pose_r.p;
+      // error_l = desired_position_l3 - pose_l.p;
+
+      KDL::Frame pose_temp = pose_r;
+      pose_temp.p.y(0);
+      error_l = pose_temp.p - pose_l.p;
+      }
+
       command_vel_r.vel = Kp*error_r;
       command_vel_l.vel = Kp*error_l;
       ROS_INFO_THROTTLE(0.1,
@@ -119,14 +185,26 @@ int main (int argc, char ** argv)
       publishers[i].publish(command_r.velocity[i]);
       }
 
+      // Publish velocities_l
       for (int i = 0; i < 7; i++){
       publishers[i+7].publish(command_l.velocity[i+7]);
       }
+    }
 
+    // this part is for calculating when its time to move to a new pose
+    counter++;
+    if(counter>400){
+      counter = 0;
+      pose_counter++;
+      ROS_INFO_STREAM(".        ~~~        .");
+      ROS_INFO_STREAM(".        ~~~        .");
+      ROS_INFO_STREAM(".time for a new pose.");
+      ROS_INFO_STREAM(".        ~~~        .");
+      ROS_INFO_STREAM(".        ~~~        .");
     }
 
     ros::spinOnce();
-    ros::Duration(0.1).sleep();
+    ros::Duration(0.02).sleep();
   }
 
   return 0;
